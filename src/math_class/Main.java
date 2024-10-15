5815e3e79a45ea21a4a66ab12956dd2a82fbc1a0
fryerj_wlhs <fryerj@wlhs.wlwv.k12.or.us>
2024-10-15 15:00:30 -0700

    theys eee min the club and h

--- src/math_class/Main.java
+++ src/math_class/Main.java
@@ -16,22 +16,40 @@ public class Main {
         // ClassName.methodName(); e.g. Math.random()
         // ClassName.staticVariableName(); e.g. Math.PI
 
+        double myPI = Math.PI;
+
+
         // abs
+        int num1 = -2;
+        int absValue = Math.abs(num1);
+        System.out.println(absValue);
 
         // pow
+        double num2 = Math.pow(3,5);
+        System.out.println((num2)); //cast to int if you wan that
 
         // sqrt
-
-
+        double num3 = Math.sqrt(80);
+        System.out.println(num3);
+        System.out.println(Math.pow(num3,2)); //round off error
 
         //create a random number in the range [0,1)
+        double random1 = Math.random();
+        System.out.println(random1);
 
         //create a random number in the range [0, n)
+        //example: [0,6)
+        double random2 = (int)(Math.random()*6);
+        //in genral just multiply by n
 
         // create a random number in the range [0,n]
+        // example [0,6)
+        double random3 = (int)(Math.random()*7);
+
+
+        //For Homework
 
         // create a random number in the range [m,n]
-
 
 
         //using the Random Class
